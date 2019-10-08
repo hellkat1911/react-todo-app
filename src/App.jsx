f01d6b78404d2775f6a2a326ac0c99c0d7eb3802
Justin Yarbrough <justin.yarbrough@twisted-rope.com>
2019-10-08 00:48:34 -0400

    Update comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import TodoContainer from './components/Container';
 
+/*
+  React project files can have a `.js` or `.jsx` extension. Most modern
+  tooling and IDEs treat them both correctly with regard to syntax highlighting,
+  linting, etc
+*/
+
 const App = () => {
   return <TodoContainer />;
 };
